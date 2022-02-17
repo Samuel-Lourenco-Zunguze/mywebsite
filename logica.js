e2fd7ad82b94ecca4448a6ba57b9f9fad68b4ec7
Samuel L. Zunguze <slourenco728@gmail.com>
2022-02-17 12:37:54 +0200

    Updates

---
+++ logica.js
@@ -0,0 +1,12 @@
+const questoes = [
+	{
+		pergunta: "como te chamas?",
+		opcoes: ["A", "B", "C", "D"],
+		resposta: 2,
+	},
+	{
+		pergunta: "como vais?",
+		opcoes: ["A", "B", "C", "D"],
+		resposta: 1,
+	},
+];
